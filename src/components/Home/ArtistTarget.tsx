86fd7b42c87e206b2ce4e75654b65d739abb63d6
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2023-03-06 09:18:26 -0600

    fixed artist target mobile

--- src/components/Home/ArtistTarget.tsx
+++ src/components/Home/ArtistTarget.tsx
@@ -5,11 +5,11 @@ import BtnDark from '../Buttons/BtnDark'
 const ArtistTarget = () => {
     return (
         <section className='flex flex-col items-center pt-10 pb-10  lg:justify-center w-full lg:pt-14 lg:pb-16'>
-            <div className='lg:flex lg:max-w-70 lg:w-full justify-between '>
-                <div className=''>
+            <div className='lg:flex lg:max-w-70 xl:w-full justify-between '>
+                <div className='flex flex-col items-center lg:items-start'>
                     <div className='flex flex-col items-center px-6 pb-4 lg:px-0 lg:items-start lg:pb-0'>
                         {/* Header wrapper */}
-                        <div className='mb-2 flex flex-col items-center lg:flex-row lg:items-end lg:gap-2'>
+                        <div className='mb-2 flex flex-col items-center md:flex-row lg:items-end md:gap-2'>
                             <h2 className='text-2.5 text-primary font-semibold'>ARTIST TARGET </h2>
                             <span className='text-1.5 lg:text-2'>by Meloroids</span>
                         </div>
@@ -20,7 +20,7 @@ const ArtistTarget = () => {
                     <img
                         src='https://images.unsplash.com/photo-1621535484902-f946de404eb4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1372&q=80'
                         alt='A rapper wearing a ski mask smoking a cigarette. Photo by David Huck on Unsplash.'
-                        className='object-cover px-6 h-48 w-full rounded  lg:hidden'
+                        className='object-cover px-6 h-48 w-full rounded md:h-88  lg:hidden'
                     />
                     <div className='px-6 pt-6 pb-8 lg:px-0 lg:max-w-660'>
                         <p className='text-1 mb-5'>With Artist Target, you can experiment with different melodies and harmonies while also learning how to emulate the sound of your favorite artists. Whether you're an experienced musician or just starting out, our feature is a fun and effective way to improve your songwriting skills.</p>
