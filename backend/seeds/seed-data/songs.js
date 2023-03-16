e81bd9a686171e7ca65a8ed5c1fd689442fe6a3f
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2023-03-16 09:23:03 -0500

    backend data application set up

---
+++ backend/seeds/seed-data/songs.js
@@ -0,0 +1,30 @@
+export const songsData = [
+  {
+    song_name: "Lucid Dreams",
+    tempo: 168,
+  },
+  {
+    song_name: "Robbery",
+    tempo: 140,
+  },
+  {
+    song_name: "You Right",
+    tempo: 130,
+  },
+  {
+    song_name: "Say So",
+    tempo: 110,
+  },
+  {
+    song_name: "Rap Star",
+    tempo: 90,
+  },
+  {
+    song_name: "Be Alone",
+    tempo: 120,
+  },
+  {
+    song_name: "No Love Lost",
+    tempo: 120,
+  },
+];
--- backend/seeds/seeds.js
+++ backend/seeds/seeds.js
@@ -3,11 +3,13 @@ import Artist from "../models/Artist.js";
 import Genre from "../models/Genre.js";
 import Key from "../models/Key.js";
 import Progression from "../models/Progression.js";
+import Song from "../models/Song.js";
 import { albumsData } from "./seed-data/albums.js";
 import { artistData } from "./seed-data/artists.js";
 import { genresData } from "./seed-data/genres.js";
 import { keysData } from "./seed-data/keys.js";
 import { progressionData } from "./seed-data/progressions.js";
+import { songsData } from "./seed-data/songs.js";
 
 export const seedDB = async () => {
   await Key.deleteMany();
@@ -15,10 +17,12 @@ export const seedDB = async () => {
   await Album.deleteMany();
   await Genre.deleteMany();
   await Progression.deleteMany();
+  await Song.deleteMany();
 
   const createdKeys = await Key.create(keysData);
   const createdGenres = await Genre.create(genresData);
   const createdArtists = await Artist.create(artistData);
   const createdAlbums = await Album.create(albumsData);
+  const createdSongs = await Song.create(songsData);
   const createdProgressions = await Progression.create(progressionData);
 };
