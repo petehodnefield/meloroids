9ba730353e1ba9797c791bc49be88827fc578796
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2023-03-23 08:48:40 -0500

    Dice reroll icon added

--- frontend/src/pages/train/random/index.js
+++ frontend/src/pages/train/random/index.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "@apollo/client";
 import { ALL_PROGRESSIONS, ALL_KEYS } from "../../../../utils/queries";
+import { randomWord } from "../../../../utils/data/words";
+import LoopFileName from "../../../components/Train/LoopFileName";
+import { Icon } from "@iconify/react";
+import Link from "next/link";
 
 const RandomTrain = () => {
+  const [loopName, setLoopName] = useState(randomWord);
   const {
     loading: progressionLoading,
     data: progressionData,
@@ -45,9 +50,65 @@ const RandomTrain = () => {
   const tempo = Math.floor(Math.random() * (200 - 60) + 60);
 
   console.log("Tempo", tempo);
+
+  const loopFileName = loopName
+    .concat(" ")
+    .concat(tempo + "bpm ")
+    .concat(key)
+    .concat(randomKey.is_major ? `Major ` : `Minor `)
+    .concat("@mongamonga_");
+
   return (
-    <div>
-      {numerals} {progression} {tempo} {key}
+    <div
+      className="bg-cover min-h-screen  flex items-start justify-center"
+      style={{
+        backgroundImage: `url(https://images.unsplash.com/photo-1514320291840-2e0a9bf2a9ae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80)`,
+      }}
+    >
+      <div className="flex flex-col items-center w-full md:py-12  lg:max-w-48 lg:justify-between">
+        {/* White bg for content */}
+        <div className="relative w-full flex flex-col items-center pt-10  bg-white md:max-w-26 md:rounded-lg md:mb-6">
+          {/* Reroll Icon */}
+          <Link href="/train/random">
+            <Icon
+              className="absolute top-4 right-4 text-1.5 hover:cursor-pointer hover:opacity-80"
+              icon="mdi:dice-6"
+            />
+          </Link>
+
+          {/* Loop Title */}
+          <h2 className="text-2.5 text-primary font-semibold mb-8">
+            {randomWord}
+          </h2>
+          {/* Data wrapper */}
+          <div className="pb-6">
+            <div className="flex flex-col items-center mb-4">
+              <h3 className="text-1 font-semibold text-medium">
+                🎼 Chords (literal):
+              </h3>
+              <p className="text-2 font-semibold">{progression}</p>
+            </div>
+            <div className="flex flex-col items-center mb-4">
+              <h3 className="text-1 font-semibold text-medium">
+                🎼 Chords (numerals):
+              </h3>
+              <p className="text-2 font-semibold">{numerals}</p>
+            </div>
+
+            <div className="flex flex-col items-center mb-4">
+              <h3 className="text-1 font-semibold text-medium">🔑 Key:</h3>
+              <p className="text-2 font-semibold">
+                {key} {randomKey.is_major ? `Major` : `Minor`}
+              </p>
+            </div>
+            <div className="flex flex-col items-center mb-4">
+              <h3 className="text-1 font-semibold text-medium">🏃🏽‍♂️ Tempo:</h3>
+              <p className="text-2 font-semibold"> {tempo} BPM</p>
+            </div>
+          </div>
+          <LoopFileName loopName={loopFileName}></LoopFileName>
+        </div>
+      </div>
     </div>
   );
 };
