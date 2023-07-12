463e5bb2f4bd62f778be02ae6e1e6a712f19fcfe
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2023-07-12 07:54:29 -0500

    Tablet media query optimized for homepage

--- frontend/src/components/Home/CTA.js
+++ frontend/src/components/Home/CTA.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 import ctaImage from "../../../public/assets/images/cta-img.png";
 const CTA = ({ authorized }) => {
   const btn =
-    "h-12 w-full xl:w-48 rounded text-1 font-semibold flex items-center justify-center";
+    "h-12 w-full md:w-48 xl:w-48 rounded text-1 font-semibold flex items-center justify-center";
   const btnPrimary = "bg-primary";
   const btnOutline = "bg-transparent border-1 border-white";
   return (
@@ -20,7 +20,7 @@ const CTA = ({ authorized }) => {
           Join the growing community of producers that are focusing on what
           truly matters. Ignite your creativity through minimalism.
         </h3>
-        <div className="flex flex-col w-full gap-4 xl:gap-8">
+        <div className="flex flex-col items-center justify-center md:flex-row w-full gap-4 xl:gap-8">
           <Link
             className={`${btn} ${btnPrimary}`}
             href={`${authorized ? "/quickie" : "/signup"}`}
--- frontend/src/components/Home/Hero.js
+++ frontend/src/components/Home/Hero.js
@@ -9,21 +9,22 @@ const Hero = ({ authorized }) => {
   const btnOutline = "bg-transparent border-1 border-white";
   return (
     <div className="relative flex justify-center flex-col items-center">
+      {/* Background Image */}
       <Image
         src={heroImage}
         alt="A guitar and keyboard floating in space"
         className="absolute top-0 left-0 h-full w-full object-cover z-0 hero__img"
       />
-      <div className="relative w-full xl:h-600 flex justify-center items-center xl:max-w-70 xl:p-12">
-        {/* Background Image */}
-        <div className="py-12 px-6 md:py-16 relative text-white text-center max-w-660 flex flex-col items-center">
-          <h1 className="text-3 font-semibold mb-6">
+      {/* Hero text */}
+      <div className="relative w-full xl:h-600 flex justify-center lg:justify-center items-center  lg:max-w-48 lg:w-full lg:px-0 ">
+        <div className="py-12 px-6 md:py-16 lg:px-0 relative text-white text-center lg:text-left max-w-660 lg:max-w-full lg:w-full flex flex-col items-center lg:items-start">
+          <h1 className="text-3 font-semibold mb-6 lg:max-w-660">
             Music Production Simplified
           </h1>
           <h2 className="text-1.5 font-medium mb-8">
             Focus on what <span className="font-bold">truly</span> matter
           </h2>
-          <h3 className="text-1.125 font-medium mb-8">
+          <h3 className="text-1.125 font-medium mb-8 lg:max-w-660">
             Meloroids is the{" "}
             <span className="font-bold ">antidote to beat block</span>, allowing
             producers to be able to focus on producing their highest quality
