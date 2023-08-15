c148794f566a87cebd376b722e2f789db79ac45c
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2023-08-15 09:09:41 -0500

    added minorKeys to algorithm

--- backend/text.js
+++ backend/text.js
@@ -20,7 +20,7 @@ const FSharpMajor = ["F#", "G#", "A#", "B", "C#", "D#", "E#"];
 const GbMajor = ["Gb", "Ab", "Bb", "Cb", "Db", "Eb", "F"];
 const GMajor = ["G", "A", "B", "C", "D", "E", "F#"];
 
-// Major Flat Keys
+// Major Keys
 const majorKeys = [
   { key: "Ab", notesInKey: ["Ab", "Bb", "C", "Db", "Eb", "F", "G"] },
   { key: "A", notesInKey: ["A", "B", "C#", "D", "E", "F#", "G#", "A"] },
@@ -37,6 +37,24 @@ const majorKeys = [
   { key: "F#", notesInKey: ["F#", "G#", "A#", "B", "C#", "D#", "E#"] },
   { key: "Gb", notesInKey: ["Gb", "Ab", "Bb", "Cb", "Db", "Eb", "F"] },
   { key: "G", notesInKey: ["G", "A", "B", "C", "D", "E", "F#"] },
+];
+
+const minorKeys = [
+  { key: "Ab", notesInKey: ["Ab", "Bb", "Cb", "Db", "Eb", "Fb", "Gb"] },
+  { key: "A", notesInKey: ["A", "B", "C", "D", "E", "F", "G"] },
+  { key: "A#", notesInKey: ["A#", "B#", "C#", "D#", "E#", "F#", "G#"] },
+  { key: "Bb", notesInKey: ["Bb", "C", "Db", "Eb", "F", "Gb", "Ab"] },
+  { key: "B", notesInKey: ["B", "C#", "D", "E", "F#", "G", "A"] },
+  { key: "C", notesInKey: ["C", "D", "Eb", "F", "G", "Ab", "Bb"] },
+  { key: "C#", notesInKey: ["C#", "D#", "E", "F#", "G#", "A", "B"] },
+  { key: "D", notesInKey: ["D", "E", "F", "G", "A", "Bb", "C"] },
+  { key: "D#", notesInKey: ["D#", "E#", "F#", "G#", "A#", "B", "C#"] },
+  { key: "Eb", notesInKey: ["Eb", "F", "Gb", "Ab", "Bb", "Cb", "Db"] },
+  { key: "E", notesInKey: ["E", "F#", "G", "A", "B", "C", "D"] },
+  { key: "F", notesInKey: ["F", "G", "Ab", "Bb", "C", "Db", "Eb"] },
+  { key: "F#", notesInKey: ["F#", "G#", "A", "B", "C#", "D", "E"] },
+  { key: "G", notesInKey: ["G", "A", "Bb", "C", "D", "Eb", "F"] },
+  { key: "G#", notesInKey: ["G#", "A#", "B", "C#", "D#", "E", "F#"] },
 ];
 
 const returnMajorKey = async (key, [...numbers]) => {
@@ -74,8 +92,50 @@ const returnMajorKey = async (key, [...numbers]) => {
     }
     // Combine note with major or minor notation
     const combine = `${progressions}${majorMinor}`;
-    console.log("combine", combine);
     return combine;
   });
 };
-returnMajorKey("E", [1, 1, 1, 4]);
+const returnMinorKey = async (key, [...numbers]) => {
+  // Find the key that matches the key argument
+  const filterKey = minorKeys.filter((keyName) => keyName.key == key);
+
+  //   Get the notes from the filtered key
+  const chordsInKey = filterKey[0].notesInKey;
+
+  //   Loop through the indexes to return the proper chords in the progression
+  numbers.forEach((number) => {
+    let majorMinor;
+
+    const progressions = chordsInKey[number - 1];
+    // Checks the index to see if it's a major or minor chord
+    switch (number) {
+      case 1:
+        majorMinor = "-";
+        break;
+      case 2:
+        majorMinor = "-";
+        break;
+      case 3:
+        majorMinor = "";
+        break;
+      case 4:
+        majorMinor = "-";
+        break;
+      case 5:
+        majorMinor = "-";
+        break;
+      case 6:
+        majorMinor = "";
+        break;
+      case 7:
+        majorMinor = "";
+        break;
+    }
+    // Combine note with major or minor notation
+    const combine = `${progressions}${majorMinor}`;
+    console.log(combine);
+    return combine;
+  });
+};
+returnMajorKey("E", [6, 1, 5, 4]);
+returnMinorKey("E", [1, 6, 7]);
