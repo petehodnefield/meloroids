3b032b28869cecb5e60bb5ccc70fa14f3fe89424
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2023-09-01 08:10:58 -0500

    Added Terms and conditions text

--- frontend/src/components/Footer/PrivacyPolicy.js
+++ frontend/src/components/Footer/PrivacyPolicy.js
@@ -8,12 +8,18 @@ const PrivacyPolicy = ({ displayClass }) => {
       className={` ${displayClass} flex flex-col items-center lg:items-start gap-2`}
     >
       <div className="flex gap-4">
-        <Link href={""} className="text-1 font-medium text-light">
+        <Link
+          href={"/privacy-policy"}
+          className="text-1 font-medium text-light"
+        >
           Privacy Policy
         </Link>
         |
-        <Link href={""} className="text-1 font-medium text-light">
-          Terms of Service
+        <Link
+          href={"/terms-and-conditions"}
+          className="text-1 font-medium text-light"
+        >
+          Terms and Conditions
         </Link>
       </div>
       <p className="text-1 font-medium text-light">
